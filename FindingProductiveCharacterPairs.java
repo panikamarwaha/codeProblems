5594aa9165a398546a1f26a07fee6031e9d834cc
Panika Marwaha <101396699+panikamarwaha@users.noreply.github.com>
2023-11-03 02:14:08 +0530

    Update and rename thirteen13.java to FindingProductiveCharacterPairs.java

--- FindingProductiveCharacterPairs.java
+++ FindingProductiveCharacterPairs.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class thirteen13 {
+public class FindingProductiveCharacterPairs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String productType = sc.nextLine();

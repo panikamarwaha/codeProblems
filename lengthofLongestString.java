0d4abfa8ff1b682be97c6c9c5b914aed13408190
Panika Marwaha <101396699+panikamarwaha@users.noreply.github.com>
2023-11-03 02:12:57 +0530

    Update and rename third3.java to lengthofLongestString.java

--- lengthofLongestString.java
+++ lengthofLongestString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class third3 {
+public class lengthofLongestString  {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         String s= sc.nextLine();

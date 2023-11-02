bcdfc197a69be970b52bbdbb9c89875b8ee3930c
Panika Marwaha <101396699+panikamarwaha@users.noreply.github.com>
2023-11-03 02:06:11 +0530

    Update CostOfGroceries.java

--- CostOfGroceries.java
+++ CostOfGroceries.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Tweleve12 {
+public class CostOfGroceries {
     public static void main(String[] args) {
         Scanner sc= new Scanner (System.in);
         int N=sc.nextInt();

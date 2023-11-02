2359e97c7f026148ebb9417952154ed6c8e6df69
Panika Marwaha <101396699+panikamarwaha@users.noreply.github.com>
2023-11-03 02:11:56 +0530

    Update and rename second2.java to RemoveStringsLexoGRaphically.java

--- RemoveStringsLexoGRaphically.java
+++ RemoveStringsLexoGRaphically.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class second2 {
+public class RemoveStringsLexoGRaphically {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n= sc.nextInt();
